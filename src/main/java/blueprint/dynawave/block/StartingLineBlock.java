8fb811d9c5e12e652f78802728fc5f91741ca4bc
flippycereal <brodiebabcock1@gmail.com>
2024-09-22 18:55:22 -0400

    Added RaceManager and changed the StartingLineBlock to account for it

--- src/main/java/blueprint/dynawave/block/StartingLineBlock.java
+++ src/main/java/blueprint/dynawave/block/StartingLineBlock.java
@@ -1,10 +1,15 @@
 package blueprint.dynawave.block;
 
+import blueprint.dynawave.course.RaceManager;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class StartingLineBlock extends Block {
     public StartingLineBlock(Settings settings) {
@@ -18,5 +23,16 @@ public class StartingLineBlock extends Block {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(CHECKPOINT);
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        if (entity instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entity;
+            int checkpointType = state.get(CHECKPOINT); // Replace with your actual property
+
+            RaceManager.getInstance().handleCheckpoint(player, checkpointType);
+        }
+        super.onSteppedOn(world, pos, state, entity);
     }
 }
